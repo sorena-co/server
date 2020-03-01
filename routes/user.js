7820e9797dd3fdc7ae3aaad7aaa7b1cc943364fa
mohammad <mo.jalili73@gmail.com>
2020-03-01 20:00:30 +0330

    update user controller.

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,15 @@ const jwt = require('jsonwebtoken');
 router.post('/register', (req, res) => {
     const user = new User(req.body);
     user.save()
-        .then(() => res.send('register success'))
+        .then((u) => {
+            const token = jwt.sign({sub: u.id}, config.secret);
+            let userWithOutPassword = u._doc;
+            delete userWithOutPassword['password'];
+            res.send({
+                token: token,
+                user: userWithOutPassword
+            });
+        })
         .catch(reason => res.send(reason));
 });
 
@@ -16,8 +24,23 @@ router.post('/login', (req, res) => {
     User.findOne({username: user.username, password: user.password})
         .then((user) => {
             const token = jwt.sign({sub: user.id}, config.secret);
+            let userWithOutPassword = user._doc;
+            delete userWithOutPassword['password'];
             res.send({
-                token: token
+                token: token,
+                user: userWithOutPassword
+            });
+        })
+        .catch(reason => res.send(reason));
+});
+
+router.get('/:username', (req, res) => {
+    User.findOne({username: req.params.username})
+        .then((user) => {
+            let u = user._doc;
+            delete u['password'];
+            res.send({
+                u
             });
         })
         .catch(reason => res.send(reason));
